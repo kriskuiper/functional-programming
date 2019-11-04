25bb6ca501c9281244e6885ec4033cf14c21f95d
Kris <kriskuiper74@gmail.com>
2019-11-04 21:42:48 +0100

    chore: simplify build

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,16 @@
 import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
 import resolve from 'rollup-plugin-node-resolve'
-import copy from 'rollup-plugin-copy'
 import { terser } from 'rollup-plugin-terser'
 
 export default {
-	input: 'src/main.js',
+	input: 'src/scripts/app.js',
 	output: {
-		file: 'build/bundle.js',
+		file: 'build/scripts/app.js',
 		format: 'iife',
-		name: 'bundle',
+		name: 'app',
 	},
 	plugins: [
-		copy({
-			targets: [
-				{ src: 'src/index.html', dest: 'build/' },
-				{ src: 'src/styles/app.css', dest: 'build/styles' }
-			],
-			hook: 'writeBundle',
-			copyOnce: true
-		}),
 		babel({
 			exclude: 'node_modules/**'
 		}),
