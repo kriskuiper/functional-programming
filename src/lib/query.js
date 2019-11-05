593c85b96ed467b6285484a89dc8cda7ad08ea6a
Kris <kriskuiper74@gmail.com>
2019-11-05 21:10:31 +0100

    feat: store query in lib

---
+++ src/lib/query.js
@@ -0,0 +1,18 @@
+export default `
+	PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+	PREFIX dc: <http://purl.org/dc/elements/1.1/>
+	PREFIX dct: <http://purl.org/dc/terms/>
+	PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+	PREFIX edm: <http://www.europeana.eu/schemas/edm/>
+	PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+
+	SELECT ?obj ?title ?img ?year ?size WHERE {
+		?sub dc:type ?obj .
+		?sub dc:title ?title .
+		?sub edm:isShownBy ?img .
+		?sub dct:created ?date .
+		?sub dct:extent ?size .
+		BIND (xsd:gYear(?date) AS ?year) .
+		FILTER (?year > xsd:gYear("1000")) .
+	} LIMIT 5
+`
