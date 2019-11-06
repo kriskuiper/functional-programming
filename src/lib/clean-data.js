d7ffd16b954aee48913a8de91481c68bf1f1a021
Kris <kriskuiper74@gmail.com>
2019-11-06 15:51:38 +0100

    feat(clean-data): return real objects

--- src/lib/clean-data.js
+++ src/lib/clean-data.js
@@ -1,7 +1,13 @@
 import formatLengthForResult from './format-length-for-result'
 
 export default function(data) {
-	return data.map(item => {
-		return formatLengthForResult(item.size.value)
-	})
+	return data
+		.map(item => ({
+			title: item.title && item.title.value,
+			img: item.img && item.img.value,
+			size: item.size && formatLengthForResult(item.size.value)
+		}))
+		.filter(item => {
+			return item.size !== null
+		})
 }
