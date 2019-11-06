62be55bd4a30a193dcde1d6cf2fc309fd08cc87e
Kris <kriskuiper74@gmail.com>
2019-11-06 15:55:28 +0100

    feat(clean-data): format year for result

--- src/lib/clean-data.js
+++ src/lib/clean-data.js
@@ -1,11 +1,15 @@
 import formatLengthForResult from './format-length-for-result'
+import formatYearForResult from './format-year-for-result'
 
 export default function(data) {
+	console.log(data)
+
 	return data
 		.map(item => ({
 			title: item.title && item.title.value,
 			img: item.img && item.img.value,
-			size: item.size && formatLengthForResult(item.size.value)
+			size: item.size && formatLengthForResult(item.size.value),
+			year: item.year && formatYearForResult(item.year.value)
 		}))
 		.filter(item => {
 			return item.size !== null
