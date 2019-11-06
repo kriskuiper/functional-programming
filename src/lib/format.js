5cb73f4748c8fa202ba623344f85ae86bfce7c51
Kris <kriskuiper74@gmail.com>
2019-11-06 15:13:02 +0100

    feat(format): make it it's own module

---
+++ src/lib/format.js
@@ -0,0 +1,9 @@
+/*
+* Implemented using the following example:
+* https://www.freecodecamp.org/news/pipe-and-compose-in-javascript-5b04004ac937/
+*/
+export default function(...functions) {
+	return (text) => functions.reduce((value, currentFunction) => {
+		return currentFunction(value)
+	}, text)
+}
