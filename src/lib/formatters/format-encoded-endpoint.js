aea33910934af7d1a86f4d11758e031c1ba3e4a9
Kris <kriskuiper74@gmail.com>
2019-11-08 18:01:13 +0100

    feat(format-encoded-query): just use encodeURIComponent

--- src/lib/formatters/format-encoded-endpoint.js
+++ src/lib/formatters/format-encoded-endpoint.js
@@ -1,8 +1,6 @@
-import encodeQuery from '../encode-query'
-
 export default function(query) {
 	const endpoint = 'https://api.data.netwerkdigitaalerfgoed.nl/datasets/ivo/NMVW/services/NMVW-21/sparql'
-	const encodedQuery = encodeQuery(query)
+	const encodedQuery = encodeURIComponent(query)
 
 	return `${endpoint}?query=${encodedQuery}&format=json`
 }
