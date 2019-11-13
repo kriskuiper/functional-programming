5608c45775149b101fd359246952cb799a2893df
Kris <kriskuiper74@gmail.com>
2019-11-13 15:18:22 +0100

    feat: delete get unique categories, let d3 take care of it

--- src/lib/get-unique-categories-from-results.js
+++
@@ -1,9 +0,0 @@
-export default function(...results) {
-	return unique(results.flat().map(result => result.category))
-}
-
-function unique(array) {
-	return array.filter((item, index, self) => {
-		return self.indexOf(item) === index
-	})
-}
