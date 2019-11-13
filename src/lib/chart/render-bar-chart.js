9db5522293b064c9d8ee2c885485a616744ebc5b
Kris <kriskuiper74@gmail.com>
2019-11-13 22:58:14 +0100

    feat(render-bar-chart): alter renderBarchart from Martijn

---
+++ src/lib/chart/render-bar-chart.js
@@ -0,0 +1,80 @@
+import * as d3 from 'd3'
+
+export default function(data, width = 1000, height = 500) {
+	const padding = 50
+	const ticks = 6
+
+	const svg = createChartBlock('.bar-chart', width, height)
+	const scale = calculateXScale(data, padding, width)
+
+	addLabelsToBars(data, svg)
+	addGridLinesToHorizontalAxis(svg, height, scale)
+	addHorizontalAxis(svg, scale, height, padding, ticks)
+	addBars(data, svg, scale, padding)
+}
+
+function createChartBlock(selector, width, height) {
+	return d3.select(selector)
+		.append('svg')
+		.attr('width', width)
+		.attr('height', height)
+}
+
+function calculateXScale(data, padding, width) {
+	return d3.scaleLinear()
+		.domain([0, d3.max(data, d => d.results.length)])
+		.range([padding, width - padding])
+}
+
+function addHorizontalAxis(svg, scale, height, padding, ticks) {
+	const horizontalAxis = d3.axisBottom(scale).ticks(ticks)
+
+	return svg.append('g')
+		.attr('transform', `translate(50, ${height - padding})`)
+		.attr('class', 'bar-chart__x-axis')
+		.call(horizontalAxis)
+		.call(g => g.select('.domain').remove())
+}
+
+function addGridLinesToHorizontalAxis(svg, height, scale) {
+	return svg.append('g')
+		.attr('class', 'bar-chart__x-axis-grid')
+		.attr('transform', `translate(50, ${height - 45})`)
+		.attr('stroke', 'grey')
+		.call(
+			createGridLines(scale)
+				.tickSize(-height)
+				.tickFormat('')
+		)
+}
+
+function addBars(data, svg, scale, padding) {
+	const barHeight = 15
+
+	svg.selectAll('rect')
+		.data(data)
+		.enter()
+		.append('rect')
+		.attr('x', padding * 2)
+		.attr('y', (d, i) => i * 50)
+		.attr('width', (d) => scale(d.results.length - 40))
+		.attr('height', barHeight)
+		.attr('class', 'bar-chart__bar')
+}
+
+function addLabelsToBars(data, svg) {
+	svg.selectAll('text')
+		.data(data)
+		.enter()
+		.append('text')
+		.text((d) => d.century)
+		.attr('x', 0)
+		.attr('y', (d, i) => (i * 50) + 10)
+		.attr('dy', 0)
+		.attr('text-anchor', 'end')
+		.attr('transform', 'translate(90, 3)')
+}
+
+function createGridLines(scale, ticks) {
+	return d3.axisBottom(scale).ticks(ticks)
+}
