a6c4717d5dbfd19f963a2a3892f8008886833bd7
Kris <kriskuiper74@gmail.com>
2019-11-13 22:57:13 +0100

    feat(app): render bar chart

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 import getCleanData from './lib/get-clean-data'
+import formatDataToCenturyResults from './lib/format-data-to-century-results'
+import renderBarChart from './lib/chart/render-bar-chart'
 
 // Have to use an iife here because we can't use await without async
 (async () => {
 	const data = await getCleanData()
+	const centuryResults = formatDataToCenturyResults(data)
 
-	console.log(data)
+	console.log(centuryResults)
+
+	renderBarChart(centuryResults)
 })()
+
+
