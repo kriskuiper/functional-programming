595d4c385648f7baf0f34d8a9f0bf859e08b71de
Kris <kriskuiper74@gmail.com>
2019-11-14 12:07:33 +0100

    feat(transform-data-to-categories): only get first five from descending

--- src/lib/transformers/transform-data-to-categories.js
+++ src/lib/transformers/transform-data-to-categories.js
@@ -1,4 +1,4 @@
-import { nest } from 'd3'
+import { nest, descending } from 'd3'
 
 export default function(results) {
 	// Thanks to Chazz for this
@@ -6,10 +6,13 @@ export default function(results) {
 		.key(result => result.category)
 		.entries(results)
 
-	const dataWithAverage = newData.map(item => ({
-		...item,
-		average: formatAverageLength(item.values)
-	}))
+	const dataWithAverage = newData
+		.map(item => ({
+			...item,
+			average: formatAverageLength(item.values)
+		}))
+		.sort((a, b) => descending(a.average, b.average))
+		.slice(0, 5)
 
 	return {
 		children: dataWithAverage
