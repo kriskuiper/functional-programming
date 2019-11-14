8fa4f8d9de52ab917a3a8eff9ebee0952ab49e88
Kris <kriskuiper74@gmail.com>
2019-11-14 11:55:59 +0100

    feat(app): render bubble and bar-chart

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
 import getCleanData from './lib/get-clean-data'
-import formatDataToCenturyResults from './lib/format-data-to-century-results'
+import transformDataToCenturyResults from './lib/transformers/transform-data-to-century-results'
+import transformDataToCategories from './lib/transformers/transform-data-to-categories'
 import renderBarChart from './lib/chart/render-bar-chart'
+import renderBubbleChart from './lib/chart/render-bubble-chart'
 
 // Have to use an iife here because we can't use await without async
 (async () => {
 	const data = await getCleanData()
-	const centuryResults = formatDataToCenturyResults(data)
-
-	console.log(centuryResults)
+	const centuryResults = transformDataToCenturyResults(data)
+	const categoryResults = transformDataToCategories(data['2001 - 2100'].results)
 
 	renderBarChart(centuryResults)
+	renderBubbleChart(categoryResults)
 })()
 
 
