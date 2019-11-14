bfcfb5d0204e125d158b4e14218cd28e648ee5a9
Kris <kriskuiper74@gmail.com>
2019-11-14 18:21:19 +0100

    feat(if-value): make it a bit more robust

--- src/lib/functional-helpers/if-value.js
+++ src/lib/functional-helpers/if-value.js
@@ -1,7 +1,10 @@
 export default function (value, fn, alternative) {
 	if (value) {
 		return fn()
+	} else if (alternative) {
+		return alternative
 	}
 
-	return alternative
+	// Nothing is literally defined so this would suffice
+	return undefined
 }
